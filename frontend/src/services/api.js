713b330f87308acc6319e451604e38fd730ce18e
SHLOK KUMAR <163283027+Shlok9027@users.noreply.github.com>
2025-07-08 20:30:45 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:4000",
+  baseURL: https://doddlesync-baackend.onrender.com,
   withCredentials: true,
 });
 
